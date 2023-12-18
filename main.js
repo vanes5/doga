0514e1e97ee0f398099bb1df07ae737513f35278
Gecse Vanessza <vanessza.gecse@gmail.com>
2023-12-18 12:15:59 +0100

    szám hozzáadás a listába

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    
+    lista = [];
+})
+
+document.getElementById("gomb").addEventListener("click", ()=>{
+    szam = document.getElementById("szam");
+    lista.add(szam);
+
+
 })
